ea57ac7b92824b9917235729f8989d9ba03e83d2
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-02 12:59:02 +0530

    ch1 problem 2

---
+++ chapter 1/problem 2.py
@@ -0,0 +1,23 @@
+# write a program to print tha table of 5
+
+a = 5*1
+b = 5*2
+c = 5*3
+d = 5*4
+e = 5*5
+f = 5*6
+g = 5*7
+h = 5*8
+i = 5*9
+j = 5*10
+
+print("5*1=",a)
+print("5*2=",b)
+print("5*3=",c)
+print("5*4=",d)
+print("5*5=",e)
+print("5*6=",f)
+print("5*7=",g)
+print("5*8=",h)
+print("5*9=",i)
+print("5*10=",j)
