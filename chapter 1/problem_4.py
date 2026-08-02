2164fcd759fca8330996a3827b127d0d29d4bb2e
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-02 13:19:11 +0530

    ch 1 problem 4

---
+++ chapter 1/problem_4.py
@@ -0,0 +1,10 @@
+# Write a python program to print the contents of a directory using the os module. Search online for the function which does that
+
+import os
+
+directory_path = '/'
+
+contents = os.listdir(directory_path)
+
+for items in contents :
+    print(items)
