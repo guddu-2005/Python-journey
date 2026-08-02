7970a60a190afd7908316c494faaade17aa5272b
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-02 15:53:26 +0530

    ch2 problem 5

---
+++ chapter 2/problem 5.py
@@ -0,0 +1,8 @@
+#  Write a python program to find an average of two numbers entered by the user.
+
+a = int(input("Enter 1st no. : "))
+b = int(input("Enter 2nd no. : "))
+
+average = (a+b)/2
+
+print("The average of" ,a ,"&" ,b , "is : ", average)
