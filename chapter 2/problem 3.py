8c3e38493c1cf11e5b2e8ee0403833929f6380c0
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-02 15:42:06 +0530

    ch2 problem 3

---
+++ chapter 2/problem 3.py
@@ -0,0 +1,11 @@
+#  Check the type of variable assigned using input() function.
+
+
+a = input('Enter 1st number : ')
+b = int(input('Enter 2nd number : '))
+c = float(input('Enter 3rd number : '))
+
+
+print (type(a))
+print (type(b))
+print (type(c))
