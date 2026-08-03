97e0de82808eabfc3d50d597b0a539df1507bfc3
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-03 11:11:46 +0530

    ch3 problem 3

---
+++ chapter 3/problem 3.py
@@ -0,0 +1,9 @@
+# Write a program to detect double space in a string.
+
+str = input('Enter your name : ')
+
+if "  " in str:
+    print("double space is present")
+
+else:
+    print("double space is not present")
