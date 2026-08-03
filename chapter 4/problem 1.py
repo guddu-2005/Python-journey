d5a997483d0e682468f208c38fb1b4f533f7fb17
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-03 23:05:44 +0530

    ch4 problem 1

--- chapter 4/problem 1.py
+++ chapter 4/problem 1.py
@@ -1,6 +1,29 @@
-list = ('ram','shyam','sita','gita',1,1,'ram')
+# Write a program to store seven fruits in a list entered by the user.
 
-li = list.count("ram")
 
-print(li)
+list = []
 
+l1 = input ('Enter 1st fruit : ')
+list.append(l1)
+
+l2 = input ('Enter 2nd fruit : ')
+list.append(l2)
+
+# l3 = input ('Enter 3rd fruit : ')
+# list.append(l3)
+
+# l4 = input ('Enter 4th fruit : ')
+# list.append(l4)
+
+# l5 = input ('Enter 5th fruit : ')
+# list.append(l5)
+
+# l6 = input ('Enter 6th fruit : ')
+# list.append(l6)
+
+# l7 = input ('Enter 7th fruit : ')
+# list.append(l7)
+
+l8 = list.extend([l1,l2])
+
+print (list)
