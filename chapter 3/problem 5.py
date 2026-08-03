a9a330c3c23c46385ae13fc9005188a12d7b083a
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-03 11:18:30 +0530

    ch3 problem 5

---
+++ chapter 3/problem 5.py
@@ -0,0 +1,9 @@
+# Write a program to format the following letter using escape sequence characters.
+
+# # letter = "Dear Sir, this python course is nice. Thanks!"
+
+
+
+letter = "Dear Sir,\nThis python course is nice,\nThanks!"
+
+print(letter)
