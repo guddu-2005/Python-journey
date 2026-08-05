0990339691e9877e73607a8d50bd34ed724e483b
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-05 12:35:58 +0530

    ch5 problem 1

---
+++ chapter 5/problem 1.py
@@ -0,0 +1,14 @@
+dictionar = {
+    "pani" : "water",
+    "chawal" : "rice",
+    "gadha" : "donkey",
+    "kitab" : "book",
+    "dahi" : "curd",
+    "khidki" : "window",
+    "darwaja" : "door"
+}
+
+
+a = input("Enter the word : ")
+
+print (f"The translation of {a} is {dictionar[a]}")
