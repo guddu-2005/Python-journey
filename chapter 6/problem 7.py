d928bfca7243a2d908793bd0a6b2bcca0cb5a172
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-08 21:55:41 +0530

    ch6 problem 7

--- chapter 6/problem 7.py
+++ chapter 6/problem 7.py
@@ -1,1 +1,12 @@
 # Write a program to find out whether a given post is talking about “Harry” or not
+
+
+
+post = input ("Enter your post : ")
+
+word = "harry"
+if word.lower() in post :
+    print ("This post talking about Harry")
+
+else :
+    print ("This post does not talking about Harry") 
