6a41fb7002ef52789e5e45da7d128867aca32ff1
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-09 21:05:55 +0530

    ch7 problem 1

---
+++ chapter 7/problem 1.py
@@ -0,0 +1,11 @@
+#  Write a program to print multiplication table of a given number using for loop.
+
+
+
+number = int(input("Enter your number : "))
+i = 0
+
+while i < 11 :
+    print (number, "X" ,i, "=", i*number)
+    i = i + 1
+
