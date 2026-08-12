4caa107def6aac7d246e1ff3fad6cfc93090199d
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-12 20:51:07 +0530

    ch8 problem 2

---
+++ chapter 8/problem 2.py
@@ -0,0 +1,12 @@
+#  Write a python program using function to convert Celsius to Fahrenheit.
+
+
+
+def convert (a) :
+    b = (a*(9/5)) + 32
+    print (b)
+
+
+a = int(input('Enter celcious : '))
+
+convert(a)
