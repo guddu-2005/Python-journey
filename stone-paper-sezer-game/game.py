bb8ed81033d5e8dee7b51584983bafbecce031b6
guddu-2005 <smrutiranjanb712@gmail.com>
2026-08-22 21:55:23 +0530

    game update 1

--- stone-paper-sezer-game/game.py
+++ stone-paper-sezer-game/game.py
@@ -1,11 +1,9 @@
 import random
 dict = {"stone" : 1, "paper" : 0, "sezer" : -1}
 reversedict = {1 : "stone", 0 : "paper", -1 : "sezer"}
-computer = random.choice([-1, 0, 1])
 
-enter = input("Enter your choice (stone, paper, sezer) : ")
 
-you = dict[enter] 
+
 '''
 stone = 1
 paper = 0
@@ -21,15 +19,24 @@ computer = -1 / you = 0 => com = -1
 me = 1, -2
 computer = 2, -1
 '''
+while True :
 
-if computer == you:
-    print("Draw!")
+    computer = random.choice([-1, 0, 1])
+    print ("\n<============================>\n")
+    enter = input("Enter your choice (stone, paper, sezer) : ")
+    you = dict[enter] 
 
-else:
-    if computer - you == 1 or computer - you == -2:
-        print ("You win")  
-        print (f"You chose {reversedict[you]}, computer chose {reversedict[computer]}")
+    if computer == you:
+        print("\nDraw!\n")
+        print (f"You chose {reversedict[you]}, computer chose {reversedict[computer]}\n")
+
 
     else:
-        print("You lose, computer win")
-        print (f"You chose {reversedict[you]}, computer chose {reversedict[computer]}")
+        if computer - you == 1 or computer - you == -2:
+            print ("\nYou win\n")  
+            print (f"You chose {reversedict[you]}, computer chose {reversedict[computer]}\n")
+
+        else:
+            print("\nYou lose, computer win\n")
+            print (f"You chose {reversedict[you]}, computer chose {reversedict[computer]}\n")
+            
